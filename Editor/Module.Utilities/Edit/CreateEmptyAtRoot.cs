eb5af768a1a9bb1e9aed5568568f82ff0169044c
Mike Wuetherick <mikew@unity3d.com>
2019-12-22 10:20:34 -0800

    fix empty gameobject at root - using local coordinates instead - also added scale & rotation as well

--- Editor/Module.Utilities/Edit/CreateEmptyAtRoot.cs
+++ Editor/Module.Utilities/Edit/CreateEmptyAtRoot.cs
@@ -11,9 +11,11 @@ namespace PixelWizards.Utilities
         {
             var go = new GameObject();
             go.name = "GameObject";
-            go.transform.position = Vector3.zero;
             go.transform.rotation = Quaternion.identity;
             go.transform.parent = Selection.activeTransform;
+            go.transform.localPosition = Vector3.zero;
+            go.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            go.transform.localScale = Vector3.one;
             Selection.activeObject = go;
         }
     }
