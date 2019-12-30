b2d22b29ce16335244290ef9c70e8a30bd9c6268
Mike Wuetherick <mikew@unity3d.com>
2019-12-30 15:31:51 -0800

    added object distribution & physics settler utilities

---
+++ Editor/Module.Utilities/Edit/DistributeEvenly.cs
@@ -0,0 +1,112 @@
+﻿using UnityEditor;
+using UnityEditor.ShortcutManagement;
+using UnityEngine;
+
+namespace MWU.Shared.Utilities
+{
+    public static class DistributeEvenly
+    {
+        public enum DistMode
+        {
+            X,
+            Y,
+            Z,
+        }
+
+        [MenuItem("Edit/Distribute/Along X", false, 2)]
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Edit/Distribute/Along X", KeyCode.Period, ShortcutModifiers.Action)]
+#endif
+        public static void DistributeEvenlyX()
+        {
+            DistributeEvenlyInternal(DistMode.X);
+        }
+
+        [MenuItem("Edit/Distribute/Along Y", false, 2)]
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Edit/Distribute/Along Y", KeyCode.Slash, ShortcutModifiers.Action)]
+#endif
+        public static void DistributeEvenlyY()
+        {
+            DistributeEvenlyInternal(DistMode.Y);
+        }
+
+        [MenuItem("Edit/Distribute/Along Z", false, 2)]
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Edit/Distribute/Along Z", KeyCode.Comma, ShortcutModifiers.Action)]
+#endif
+        public static void DistributeEvenlyZ()
+        {
+            DistributeEvenlyInternal(DistMode.Z);
+        }
+
+        private static void DistributeEvenlyInternal(DistMode mode)
+        {
+            var selectedObjects = Selection.gameObjects;
+            if (selectedObjects.Length < 1)
+                return;
+
+            var sourcePosition = selectedObjects[0];
+            Undo.RegisterCompleteObjectUndo(selectedObjects, "Distribute Evenly");
+
+            var previousSize = Vector3.zero;
+            Vector3 nextPos = sourcePosition.transform.position;
+            foreach (var selectedObject in selectedObjects)
+            {
+                var collider = selectedObject.AddComponent<BoxCollider>();
+                if (selectedObject.transform.childCount > 0)
+                    GrowToFitChildren(selectedObject, selectedObject);
+                // bump the subsequent objects
+                if( selectedObject != selectedObjects[0])
+                {
+                    nextPos += collider.bounds.extents;
+                    nextPos.x += (collider.bounds.extents.x * 0.25f);   // add a bit of buffer between them
+                }
+
+                switch( mode )
+                {
+                    case DistMode.X:
+                        {
+                            selectedObject.transform.position = new Vector3(nextPos.x, sourcePosition.transform.position.y, sourcePosition.transform.position.z);
+
+                            break;
+                        }
+                    case DistMode.Y:
+                        {
+                            selectedObject.transform.position = new Vector3(sourcePosition.transform.position.x, nextPos.y, sourcePosition.transform.position.z);
+
+                            break;
+                        }
+                    case DistMode.Z:
+                        {
+                            selectedObject.transform.position = new Vector3(sourcePosition.transform.position.x, sourcePosition.transform.position.y, nextPos.z);
+
+                            break;
+                        }
+                }
+                nextPos = selectedObject.transform.position + collider.bounds.extents;
+
+                GameObject.DestroyImmediate(collider);
+            }
+        }
+
+        public static void GrowToFitChildren(GameObject thisGO, GameObject parent)
+        {
+            var collider = parent.GetComponent<BoxCollider>();
+            // need to take into account the size of any children as well
+            foreach (Transform child in thisGO.transform)
+            {
+                var childCollider = child.gameObject.AddComponent<BoxCollider>();
+                var bounds = collider.bounds;
+                bounds.Encapsulate(childCollider.bounds);
+                collider.size = bounds.size;
+                GameObject.DestroyImmediate(childCollider);
+
+                if( child.childCount > 0)
+                {
+                    GrowToFitChildren(child.gameObject, parent);
+                }
+            }
+        }
+    }
+}
---
+++ Editor/Module.Utilities/Edit/PhysicsSettler.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.ShortcutManagement;
+
+namespace MWU.Shared.Utilities
+{
+
+    // This causes the class' static constructor to be called on load and on starting playmode
+    [InitializeOnLoad]
+    class PhysicsSettler
+    {
+        // only ever register once
+        static bool registered = false;
+
+        // are we actively settling physics in our scene
+        static bool active = false;
+
+        // the work list of rigid bodies we can find loaded up
+        static Rigidbody[] workList;
+
+        // we need to disable auto simulation to manually tick physics
+        static bool cachedAutoSimulation;
+
+        // how long do we run physics for before we give up getting things to sleep
+        const float timeToSettle = 10f;
+
+        // how long have we been running
+        static float activeTime = 0f;
+
+        // this is the static constructor called by [InitializeOnLoad]
+        static PhysicsSettler()
+        {
+            if (!registered)
+            {
+                // hook into the editor update
+                EditorApplication.update += Update;
+
+                // and the scene view OnGui
+                SceneView.duringSceneGui += OnSceneGUI;
+                registered = true;
+            }
+        }
+
+        // let users turn on 
+        [MenuItem("Edit/Settle Physics")]
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Edit/Settle Physics", KeyCode.Q, ShortcutModifiers.Action | ShortcutModifiers.Alt)]
+#endif
+        static void Activate()
+        {
+            if (!active)
+            {
+                active = true;
+
+                // Normally avoid Find functions, but this is editor time and only happens once
+                workList = Object.FindObjectsOfType<Rigidbody>();
+
+                // we will need to ensure autoSimulation is off to manually tick physics
+                cachedAutoSimulation = Physics.autoSimulation;
+                activeTime = 0f;
+
+                // make sure that all rigidbodies are awake so they will actively settle against changed geometry.
+                foreach (Rigidbody body in workList)
+                {
+                    body.WakeUp();
+                }
+            }
+        }
+
+        // grey out the menu item while we are settling physics
+        [MenuItem("Edit/Settle Physics", true)]
+        static bool checkMenu()
+        {
+            return !active;
+        }
+
+        static void Update()
+        {
+            if (active)
+            {
+                activeTime += Time.deltaTime;
+
+                // make sure we are not autosimulating
+                Physics.autoSimulation = false;
+
+                // see if all our 
+                bool allSleeping = true;
+                foreach (Rigidbody body in workList)
+                {
+                    if (body != null)
+                    {
+                        allSleeping &= body.IsSleeping();
+                    }
+                }
+
+                if (allSleeping || activeTime >= timeToSettle)
+                {
+                    Physics.autoSimulation = cachedAutoSimulation;
+                    active = false;
+                }
+                else
+                {
+                    Physics.Simulate(Time.deltaTime);
+                }
+            }
+        }
+
+        static void OnSceneGUI(SceneView sceneView)
+        {
+            if (active)
+            {
+                Handles.BeginGUI();
+                Color cacheColor = GUI.color;
+                GUI.color = Color.red;
+                GUILayout.Label("Simulating Physics.", GUI.skin.box, GUILayout.Width(200));
+                GUILayout.Label(string.Format("Time Remaining: {0:F2}", (timeToSettle - activeTime)), GUI.skin.box, GUILayout.Width(200));
+                Handles.EndGUI();
+
+                foreach (Rigidbody body in workList)
+                {
+                    if (body != null)
+                    {
+                        bool isSleeping = body.IsSleeping();
+                        if (!isSleeping)
+                        {
+                            GUI.color = Color.green;
+                            Handles.Label(body.transform.position, "SIMULATING");
+                        }
+                    }
+                }
+                GUI.color = cacheColor;
+            }
+        }
+    }
+}
