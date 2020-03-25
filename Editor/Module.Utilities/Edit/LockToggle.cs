5b745730636a0b5b9913cc5005227253777d146f
Mike Wuetherick <mikew@unity3d.com>
2020-03-24 20:05:35 -0700

    added window toggle lock helper

---
+++ Editor/Module.Utilities/Edit/LockToggle.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+#if UNITY_2019_1_OR_NEWER
+using UnityEditor.ShortcutManagement;
+#endif
+using UnityEngine;
+
+/// <summary>
+/// Inspired by https://forum.unity.com/threads/shortcut-key-for-lock-inspector.95815/#post-5013983
+/// modded from: https://github.com/rfadeev/pump-editor/wiki/Lock-Toggle-Shortcuts
+/// 
+/// Provides toggle locking for various windows in the editor:
+/// - Scene Hierarchy
+/// - Project Window
+/// - Inspector
+/// - Timeline Window
+/// </summary>
+namespace PixelWizards.Utilities
+{
+    public static class LockToggle
+    {
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Window/Toggle Lock Focused Window", KeyCode.W, ShortcutModifiers.Action)]
+#else
+        [MenuItem("Window/Pump Editor/Tools/Toggle Lock Focused Window %w")]
+#endif
+        private static void ToggleLockFocusedWindow()
+        {
+            ToggleLockEditorWindow(EditorWindow.focusedWindow);
+        }
+
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Window/Toggle Lock Mouse Over Window", KeyCode.E, ShortcutModifiers.Action)]
+#else
+        [MenuItem("Window/Pump Editor/Tools/Toggle Lock Mouse Over Window %e")]
+#endif
+        private static void ToggleLockMouseOverWindow()
+        {
+            ToggleLockEditorWindow(EditorWindow.mouseOverWindow);
+        }
+
+#if UNITY_2019_1_OR_NEWER
+        [Shortcut("Window/Toggle Lock All Windows", KeyCode.W, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
+#else
+        [MenuItem("Window/Pump Editor/Tools/Toggle Lock All Windows %#w")]
+#endif
+        private static void ToggleLockAllWindows()
+        {
+            var allWindows = Resources.FindObjectsOfTypeAll<EditorWindow>();
+            foreach (var editorWindow in allWindows)
+            {
+                ToggleLockEditorWindow(editorWindow);
+            }
+        }
+
+        private static void ToggleLockEditorWindow(EditorWindow editorWindow)
+        {
+            var editorAssembly = Assembly.GetAssembly(typeof(Editor));
+            var projectBrowserType = editorAssembly.GetType("UnityEditor.ProjectBrowser");
+            var inspectorWindowType = editorAssembly.GetType("UnityEditor.InspectorWindow");
+            var sceneHierarchyWindowType = editorAssembly.GetType("UnityEditor.SceneHierarchyWindow");
+            var timelineWindowType = Type.GetType("UnityEditor.Timeline.TimelineWindow, Unity.Timeline.Editor");
+
+            var editorWindowType = editorWindow.GetType();
+            if (editorWindowType == projectBrowserType)
+            {
+                // Unity C# reference: https://github.com/Unity-Technologies/UnityCsReference/blob/c6ec7823//Editor/Mono/ProjectBrowser.cs#L113
+                var propertyInfo = projectBrowserType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                var value = (bool)propertyInfo.GetValue(editorWindow);
+                propertyInfo.SetValue(editorWindow, !value);
+            }
+            else if (editorWindowType == inspectorWindowType)
+            {
+                // Unity C# reference: https://github.com/Unity-Technologies/UnityCsReference/blob/c6ec7823//Editor/Mono/Inspector/InspectorWindow.cs##L492
+                var propertyInfo = inspectorWindowType.GetProperty("isLocked");
+
+                var value = (bool)propertyInfo.GetValue(editorWindow);
+                propertyInfo.SetValue(editorWindow, !value);
+            }
+            else if (editorWindowType == sceneHierarchyWindowType)
+            {
+                // Unity C# reference: https://github.com/Unity-Technologies/UnityCsReference/blob/c6ec7823/Editor/Mono/SceneHierarchyWindow.cs#L34
+                var sceneHierarchyPropertyInfo = sceneHierarchyWindowType.GetProperty("sceneHierarchy");
+                var sceneHierarchy = sceneHierarchyPropertyInfo.GetValue(editorWindow);
+
+                // Unity C# reference: https://github.com/Unity-Technologies/UnityCsReference/blob/c6ec7823/Editor/Mono/SceneHierarchy.cs#L88
+                var sceneHierarchyType = editorAssembly.GetType("UnityEditor.SceneHierarchy");
+                var propertyInfo = sceneHierarchyType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.NonPublic);
+
+                var value = (bool)propertyInfo.GetValue(sceneHierarchy);
+                propertyInfo.SetValue(sceneHierarchy, !value);
+            }
+            else if ( editorWindowType == timelineWindowType)
+            {
+                var timelineWindow = Resources.FindObjectsOfTypeAll(timelineWindowType)[0] as EditorWindow;
+                var propertyInfo = timelineWindow.GetType().GetProperty("locked");
+                var value = (bool)propertyInfo.GetValue(timelineWindow, null);
+                propertyInfo.SetValue(timelineWindow, !value, null);
+            }
+            else
+            {
+                return;
+            }
+
+            editorWindow.Repaint();
+        }
+    }
+}
