d998bb02b374ef4eb941d1414d08a0efe348c4be
Mike Wuetherick <mikew@unity3d.com>
2021-06-09 10:10:31 -0700

    application set loading priority utility

---
+++ Runtime/SceneFramework/SceneLoading/SetLoadingPriority.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelWizards.Utilities
+{
+    /// <summary>
+    /// Allows you to define the loading priority for async loading
+    /// </summary>
+    public class SetLoadingPriority : MonoBehaviour
+    {
+        [Header("Allows you to define the loading priority for async loading")]
+        public ThreadPriority loadingPriority = ThreadPriority.Low;
+
+        private void Awake()
+        {
+            Application.backgroundLoadingPriority = loadingPriority;
+        }
+    }
+}
