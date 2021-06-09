3f55f95ea028481a8b7cd4603868db4280a3c136
Mike Wuetherick <mikew@unity3d.com>
2021-06-09 10:09:01 -0700

    adding editor note to library

---
+++ Editor/EditorNote/EditorNoteInspector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PixelWizards.Utilities
+{
+
+    [CustomEditor(typeof(EditorNote))]
+    public class EditorNoteInspector : Editor
+    {
+        private static EditorNote note;
+
+        private void OnEnable()
+        {
+            note = (EditorNote)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            GUILayout.Label("Editor Note", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+
+            note.m_Text = GUILayout.TextArea(note.m_Text);
+
+            EditorGUILayout.Space();
+        }
+    }
+}
